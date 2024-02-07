e6bb5c8c187e4786b260a114f2cf5ae54729f466
jeanluciano <jeanluciano@mailbox.org>
2024-02-07 14:00:37 -0600

    semaphore changes

--- arq/connections.py
+++ arq/connections.py
@@ -251,6 +251,9 @@ class ArqRedisCluster(RedisCluster):  # type: ignore
     def pipeline(self, transaction: Optional[Any] = None, shard_hint: Optional[Any] = None) -> ClusterPipeline:
         return ArqRedisClusterPipeline(self)
 
+    async def close(self, close_connection_pool: Optional[bool] = None) -> None:
+        await self.aclose()
+
 
 class ArqRedisClusterPipeline(ClusterPipeline):  # type: ignore
     def __init__(self, client: RedisCluster) -> None:
